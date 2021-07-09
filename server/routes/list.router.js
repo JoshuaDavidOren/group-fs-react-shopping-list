521f486c0f03f9473e58cdecf715c299d4118569
Joshua Oren <joshuadavidoren@gmail.com>
2021-07-09 10:46:00 -0500

    DELETE route for one item/list set up

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -5,4 +5,38 @@ const pool = require('../modules/pool.js');
 // TODO - Add routes here...
 
 
+router.delete('/', (req, res) => {
+
+    console.log(req.params);
+    qText = `DELETE * FROM "list";`
+
+    pool.query(qText)
+        .then(dbResponse => {
+            console.log('DELETEed list data');
+            res.sendStatus(201);
+        })
+        .catch(err => {
+            console.log('DELETE list error', err);
+            res.sendStatus(500);
+        });
+});
+
+router.delete("/:id", (req, res) => {
+    const deleteId = req.params.id;
+    console.log(req.params);
+    qText = `DELETE FROM "list" WHERE ID = $1;`;
+
+    pool.query(qText, [deleteId])
+        .then((dbResponse) => {
+            console.log("DELETEed single item", { deleteID });
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log("DELETE single item error", err);
+            res.sendStatus(500);
+        });
+});
+
+
+
 module.exports = router;
