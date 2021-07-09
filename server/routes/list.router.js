2d280941cfa46e3fea7be405dd9c9740dd2c88ee
Sagethh <tate.feige@gmail.com>
2021-07-09 10:41:30 -0500

    post

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -7,15 +7,15 @@ const pool = require('../modules/pool.js');
 router.post('/', (reqs, res) => {
     let newItem = req.body;
     console.log(`Attmepting to post ${newItem}`);
-    let qText = `INSERT INTO "fs-react-shopping" ("name", quantity, "units", "isBought") VALUES ($1, $2, $3, $4);`;
-    pool.query(qText, [newItem.name, newItem.quantity, newItem.units, false])
+    let qText = `INSERT INTO "fs-react-shopping" ("name", "quantity", "unit", "isBought") VALUES ($1, $2, $3, $4);`;
+    pool.query(qText, [newItem.name, newItem.quantity, newItem.unit, false])
     .then(res => {
         res.sendStatus(200);
     })
     .catch(error => {
         console.log(error);
         res.sendStatus(500);
-    })
-})
+    });
+});
 
 module.exports = router;
