6a7baeb8e1aabec1d48d8e76f0f403365e8845d7
Joshua Oren <joshuadavidoren@gmail.com>
2021-07-09 10:57:36 -0500

    axios delete calls made

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,26 @@ import React from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
+const deleteItem = () => {
+    axios.delete('/item', {item: id})
+    .then(response => {
+        //call GET function
+    })
+    .catch(err => {
+        console.log('error deleting item app.jsx', err);
+    })
+}
+
+const deleteList = () => {
+    axios.delete('/')
+    .then(response => {
+        //call GET function
+    })
+    .catch(err => {
+        console.log('error deleting list app.jsx', err);
+    })
+}
+
 
 function App() {
     return (
