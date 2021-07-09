7fb24d8ef11c30cf39b875085d3490b7f9694f99
Matthew Bouc <boucgm@gmail.com>
2021-07-09 10:21:34 -0500

    dtable for database

--- database.sql
+++ database.sql
@@ -1,2 +1,17 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+-- Database name:  fs-react-shopping
+
+CREATE TABLE list (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(80) NOT NULL,
+    "quantity" NUMERIC NOT NULL,
+    "unit" VARCHAR(20),
+    "isBought" BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO list ("name", "quantity", "unit")
+	VALUES ('eggs', 2, 'dozen'), ('milk', 2.5, 'cups');
+	
+DROP TABLE list;
