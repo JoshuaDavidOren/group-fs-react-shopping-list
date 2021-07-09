60c14746ff100c06828a0746629f79ed98be2ee1
Kong Yang <kpyang2414@gmail.com>
2021-07-09 10:28:11 -0500

    finished creating routerput

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -1,8 +1,21 @@
+const { query } = require('express');
 const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
+
+router.put('/', (req,res) => {
+    const itemId = req.params.id;
+    const queryText = `UPDATE table SET "isBought"= !isBought WHERE id =$1;`;
+    pool.query(queryText, [itemId])
+    .then(response => {
+        console.log('Response inside router.put'. response);
+    }).catch(error => {
+        console.log('error inside router.put', error);
+    });
+})
+
 
 
 module.exports = router;
